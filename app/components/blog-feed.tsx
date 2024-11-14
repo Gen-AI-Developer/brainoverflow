9c7f6f08c014bb7de103d2f669e60760900fb27f
SafdarAliShah <programmersafdar@live.com>
2024-11-14 23:21:48 +0500

    Project: Images is now Working

--- app/components/blog-feed.tsx
+++ app/components/blog-feed.tsx
@@ -17,81 +17,81 @@ const blogPosts: BlogPost[] = [
     title: "A Continually Unfolding History — Hillview by Made by Hand",
     description:
       "A single building occupies the hillside at Hillview, a historic 240-hectare former sheep farm on Tasmania's Bruny Island...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Architecture"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 2,
     title: "Cognitive Dissonance Theory: Crash Course for UX Designers",
     description:
       "We all like to think of ourselves in certain ways. We consider ourselves to be truthful, hard-working, health-conscious...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Product", "Research", "Frameworks"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 3,
     title: "How Remote Work Drastically Improved My Design Skills",
     description:
       "Remote working might not only be feasible but beneficial. Stanford University found that remote employees were 13...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Research"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 4,
     title: "Improve Your UI Design Skills and Develop an Eye for Design",
     description:
       "The design industry is constantly evolving, but good design is timeless. Learn how to quickly develop an eye for UI...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Tools", "Research"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 5,
     title: "Exclusive Interview with Designer, Jasmin Chew",
     description:
       "Jasmin Chew is a 23-year-old photographer and photo editor from Toronto, Ontario. She has worked with Spotify...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Research", "Interview"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 6,
     title: "The Design Dilemma: Is Best UX Practice the Enemy of Creativity",
     description:
       "Frankie Sullivan explores the 'Design Dilemma' – what happens when best practice UX design clashes with creative...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Theory", "Research"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 7,
     title: "Conversations with Makr & Co.",
     description:
       "We sat down with London's fast-growing brand and product design studio, Makr & Co. to find out how they've used Untitled UI to 2x their revenue.",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Research", "Interview"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 8,
     title: "A Relentless Pursuit of Perfection in Product Design",
     description:
       '"I began to notice that there was a sharp contrast between well-made, crafted products and poorly made ones, and...',
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Product", "Research", "Frameworks"],
-    link: blogimage.src,
+    link: "#",
   },
   {
     id: 9,
     title: "How to Run a Successful Business With Your Partner",
     description:
       "Starting a business with your spouse or significant other is an exciting but delicate process and requires a great deal...",
-    image: "/placeholder.svg?height=200&width=300",
+    image: blogimage.src,
     categories: ["Design", "Research"],
-    link: blogimage.src,
+    link: "#",
   },
 ];
 
